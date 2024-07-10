9a343f3bf33db4b85afba8a474dbaf386102d8e0
Ruslan <axelezgames@gmail.com>
2024-07-10 21:00:47 +0500

    fix

--- src/components/event.jsx
+++ src/components/event.jsx
@@ -2,16 +2,16 @@ function Event(props) {
   return (
     <li className={"event" + (props.slim ? " event_slim" : "")}>
       <button className="event__button">
-        {/* <span
+        <span
           className={`event__icon event__icon_${props.icon}`}
           role="img"
           aria-label={props.iconLabel}
-        ></span> */}
-        <img
+        ></span>
+        {/* <img
           src={`./assets/icon_${props.icon}.svg`}
           alt={`${props.label}`}
           className={`event__icon`}
-        />
+        /> */}
         <h4 className="event__title">{props.title}</h4>
         {props.subtitle && (
           <span className="event__subtitle">{props.subtitle}</span>
