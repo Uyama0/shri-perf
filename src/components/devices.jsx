1fd977d716341ba80afdc66f38e5db26ed3440fe
Ruslan <axelezgames@gmail.com>
2024-07-10 23:44:08 +0500

    fix

--- src/components/devices.jsx
+++ src/components/devices.jsx
@@ -148,9 +148,8 @@ for (let i = 0; i < 6; ++i) {
 const Devices = () => {
   const ref = useRef();
   const initedRef = useRef(false);
-  const [activeTab, setActiveTab] = useState("");
+  const [activeTab, setActiveTab] = useState("all");
   const [hasRightScroll, setHasRightScroll] = useState(false);
-
   useEffect(() => {
     if (!activeTab && !initedRef.current) {
       initedRef.current = true;
@@ -220,25 +219,20 @@ const Devices = () => {
       </div>
 
       <div className="section__panel-wrapper" ref={ref}>
-        {TABS_KEYS.map((key) => (
-          <div
-            key={key}
-            role="tabpanel"
-            className={
-              "section__panel" +
-              (key === activeTab ? "" : " section__panel_hidden")
-            }
-            aria-hidden={key === activeTab ? "false" : "true"}
-            id={`panel_${key}`}
-            aria-labelledby={`tab_${key}`}
-          >
-            <ul className="section__panel-list">
-              {TABS[key].items.map((item, index) => (
-                <Event key={index} {...item} />
-              ))}
-            </ul>
-          </div>
-        ))}
+        <div
+          key={activeTab}
+          role="tabpanel"
+          className="section__panel"
+          aria-hidden="false"
+          id={`panel_${activeTab}`}
+          aria-labelledby={`tab_${activeTab}`}
+        >
+          <ul className="section__panel-list">
+            {TABS[activeTab].items.map((item, index) => (
+              <Event key={index} {...item} />
+            ))}
+          </ul>
+        </div>
         {hasRightScroll && (
           <div className="section__arrow" onClick={onArrowCLick}></div>
         )}
