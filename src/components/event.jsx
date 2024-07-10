b26e1082f8b848cfd13448d89674d19ac9887e99
Ruslan <axelezgames@gmail.com>
2024-07-10 15:19:12 +0500

    fix

--- src/components/event.jsx
+++ src/components/event.jsx
@@ -12,15 +12,14 @@ function Event(props) {
       onSize({ width, height });
     }
   });
-
   return (
     <li ref={ref} className={"event" + (props.slim ? " event_slim" : "")}>
       <button className="event__button">
-        <span
-          className={`event__icon event__icon_${props.icon}`}
-          role="img"
-          aria-label={props.iconLabel}
-        ></span>
+        <img
+          src={`src/assets/icon_${props.icon}.svg`}
+          alt={`${props.label}`}
+          className={`event__icon`}
+        />
         <h4 className="event__title">{props.title}</h4>
         {props.subtitle && (
           <span className="event__subtitle">{props.subtitle}</span>
