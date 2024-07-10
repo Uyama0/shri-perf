497c05367d0426bc22919d2d499412371bad041c
Ruslan <axelezgames@gmail.com>
2024-07-10 15:28:06 +0500

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Footer from "./components/footer";
 import Header from "./components/header";
 import Mainer from "./components/mainer";
+import List from "./components/list";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <Mainer />
       <Footer />
+      {/* <List /> */}
     </div>
   );
 }
