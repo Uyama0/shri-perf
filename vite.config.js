fa4edbbe9718d9f1ade71e14b080fcc7de25cc4d
Ruslan <axelezgames@gmail.com>
2024-07-11 15:15:20 +0500

    prel

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import compression from "vite-plugin-compression";
-import preload from "vite-plugin-preload";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/shri-perf/",
-  plugins: [react(), compression({ algorithm: "brotliCompress" }), preload()],
+  plugins: [react(), compression({ algorithm: "brotliCompress" })],
   build: {
     minify: "terser",
     rollupOptions: {
